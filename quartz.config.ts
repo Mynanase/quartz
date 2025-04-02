0e8b4416f0491cba7aaf3d82546309a56bee077b
qttao <qttao@outlook.com>
2025-04-03 00:48:45 +0800

    [CONFIG] update day

--- quartz.config.ts
+++ quartz.config.ts
@@ -18,7 +18,7 @@ const config: QuartzConfig = {
     locale: "en-US",
     baseUrl: "blog.qttao.net",
     ignorePatterns: ["private", "templates", ".obsidian"],
-    defaultDateType: "modified",
+    defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
