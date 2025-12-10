1ca2d65c3225d8bccd67706a6bfe02f63d1cf766
qttao <qttao@outlook.com>
2025-12-11 03:20:09 +0800

    feat: update page layouts to include DesktopOnly components for improved user experience

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -38,7 +38,8 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
+    Component.DesktopOnly(Component.Links()),
+    Component.DesktopOnly(Component.RecentNotes()),
   ],
   right: [
     Component.Graph(),
@@ -62,7 +63,7 @@ export const defaultListPageLayout: PageLayout = {
         { Component: Component.Darkmode() },
       ],
     }),
-    Component.Explorer(),
+    Component.DesktopOnly(Component.RecentNotes()),
   ],
   right: [],
 }
